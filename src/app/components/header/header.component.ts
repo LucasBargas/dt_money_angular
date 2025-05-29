5a374395a0ce8c6aa5f94ff1c5d3d1dea4c3f479
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-29 16:38:23 -0300

    Header - developed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { ContainerComponent } from "../container/container.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ContainerComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  private themeService = inject(ThemeService);
+  theme = this.themeService.getTheme();
 }
