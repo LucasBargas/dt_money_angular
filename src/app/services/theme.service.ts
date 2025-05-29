597e9a6c60a269fb7b55e0e6b770a52281101836
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-29 12:00:29 -0300

    SCSS variables - configured

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,17 +1,30 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 
 export class ThemeService {
-  private theme = signal<'light' | 'dark'>('dark');
+  private theme = signal<'light' | 'dark'>(this.loadTheme());
 
-  constructor() {}
+  constructor() {
+    // Update the body class always the theme changes
+    effect(() => {
+      const current = this.theme();
+      document.body.classList.remove('light', 'dark');
+      document.body.classList.add(current);
+    });
+  }
 
   setTheme(value: 'light' | 'dark') {
+    localStorage.setItem('theme', value);
     this.theme.set(value);
   }
 
   getTheme() {
     return this.theme;
+  }
+
+  private loadTheme(): 'light' | 'dark' {
+    const saved = localStorage.getItem('theme');
+    return saved === 'dark' ? 'dark' : 'light'; // light is default
   }
 }
