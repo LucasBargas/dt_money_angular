8d4074c5eeef92f6f2233f622721cab2dd656590
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-29 13:32:20 -0300

    Header - in development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { ToggleThemeButtonComponent } from "./components/toggle-theme-button/toggle-theme-button.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, ToggleThemeButtonComponent],
+  imports: [CommonModule, RouterOutlet, ToggleThemeButtonComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
