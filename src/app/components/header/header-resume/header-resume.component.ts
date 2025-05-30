85851ff68d28639ce71121153795c755b4972b09
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-29 23:38:00 -0300

    Header resume - in development

--- src/app/components/header/header-resume/header-resume.component.ts
+++ src/app/components/header/header-resume/header-resume.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
+import { TransactionsResumeService } from '../../../services/transactionsResume.service';
+import { TransactionsService } from '../../../services/transactions.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header-resume',
@@ -9,7 +12,35 @@ import { ThemeService } from '../../../services/theme.service';
   templateUrl: './header-resume.component.html',
   styleUrl: './header-resume.component.scss'
 })
-export class HeaderResumeComponent {
+export class HeaderResumeComponent implements OnInit {
   private themeService = inject(ThemeService);
   theme = this.themeService.getTheme();
+  private transactionsResumeService = inject(TransactionsResumeService);
+  transactionsResume = this.transactionsResumeService.getTransactions();
+  totalSales = 0;
+  totalExpenses = 0;
+  private subscription?: Subscription;
+
+  constructor(private transactionsService: TransactionsService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.transactionsService.getTransactions().subscribe((transactions) => {
+      this.transactionsResumeService.setTransactions(transactions);
+
+      this.totalSales = this.calculate('Venda');
+      this.totalExpenses = this.calculate('Gasto');
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  calculate(type: string): number {
+    const resume = this.transactionsResume()
+      .filter(t => t.transactionType === type)
+      .reduce((acc, curr) => acc + Number(curr.amount), 0);
+
+    return resume;
+  }
 }
