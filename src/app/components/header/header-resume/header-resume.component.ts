28245edc3cc9bdd6005df4df2ea60d9a94f8e60e
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-06-03 21:16:31 -0300

    Header resume - bug fixed

--- src/app/components/header/header-resume/header-resume.component.ts
+++ src/app/components/header/header-resume/header-resume.component.ts
@@ -1,13 +1,19 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ChangeDetectorRef,
+  inject
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
 import { TransactionsResumeService } from '../../../services/transactionsResume.service';
 import { BrlCurrencyPipe } from '../../../pipes/brl-currency.pipe';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { register } from 'swiper/element/bundle';
-import type { Swiper } from 'swiper/types';
 
 @Component({
   selector: 'app-header-resume',
@@ -18,49 +24,70 @@ import type { Swiper } from 'swiper/types';
   styleUrl: './header-resume.component.scss'
 })
 export class HeaderResumeComponent implements AfterViewInit {
-  @ViewChild('swiperEl') swiperEl!: ElementRef;
+  @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLDivElement>;
+
   private _themeService = inject(ThemeService);
   theme = this._themeService.theme;
+
   private _transactionsResumeService = inject(TransactionsResumeService);
   totalSales = this._transactionsResumeService.totalSales;
   totalExpenses = this._transactionsResumeService.totalExpenses;
+
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
-  swiper!: Swiper;
-  slidePerView = 2
 
-  constructor() {
-    register();
-  }
+  position = 0;
+  private _active = 0;
+  calc = 2;
+  totalSlides = 3;
+
+  constructor(private cdr: ChangeDetectorRef) { }
 
   @HostListener('window:resize')
   onResize() {
-    this.adjustSlidesPerView();
-    this.updateSwiperAttributes();
+    this._adjustSlidesPerView();
   }
 
   ngAfterViewInit(): void {
-    this.adjustSlidesPerView();
-    this.updateSwiperAttributes();
+    this._adjustSlidesPerView();
+    this.cdr.detectChanges();
   }
 
-  private updateSwiperAttributes(): void {
-    if (this.swiperEl) {
-      const el = this.swiperEl.nativeElement;
-      this.swiper = el.swiper;
-      el.setAttribute('slides-per-view', this.slidePerView.toString());
+  private _updatePosition(): void {
+    if (this.sliderRef) {
+      const slideElement = this.sliderRef.nativeElement.querySelector('.header-resume-area-single');
+      const slideWidth = slideElement?.getBoundingClientRect().width || 0;
+      this.position = -(slideWidth * this._active);
+      this.cdr.markForCheck();
     }
   }
 
-  private adjustSlidesPerView(): void {
-    if (window.innerWidth <= 640) {
-      this.slidePerView = 1;
-    } else if (window.innerWidth <= 768) {
-      this.slidePerView = 2;
-    } else if (window.innerWidth <= 1536) {
-      this.slidePerView = 3;
-    } else {
-      this.slidePerView = 3;
-    }
+  clickOnNext() {
+    const maxIndex = this.totalSlides - this.calc;
+    this._active = this._active < maxIndex ? this._active + 1 : 0;
+    this._updatePosition();
   }
+
+  clickOnPrev() {
+    const maxIndex = this.totalSlides - this.calc;
+    this._active = this._active > 0 ? this._active - 1 : maxIndex;
+    this._updatePosition();
+  }
+
+private _adjustSlidesPerView(): void {
+  if (window.innerWidth <= 640) {
+    this.calc = 1;
+  } else if (window.innerWidth <= 768) {
+    this.calc = 2;
+  } else {
+    this.calc = 3;
+  }
+
+  const maxIndex = this.totalSlides - this.calc;
+  if (this._active > maxIndex) {
+    this._active = maxIndex >= 0 ? maxIndex : 0;
+  }
+
+  this._updatePosition();
+}
 }
